31afe09f113b3f4de36156aab6cd6b80c13da580
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-14 16:07:45 +0100

    selecting a post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,30 +8,50 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedpostId: null
     }
 
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(res => {
+                const posts = res.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Cele'
+                    }
+                });
                 this.setState({
-                    posts: res.data
+                    posts: updatedPosts
                 })
             });
+    }
+
+    postSelectedHandler = (id) =>{
+        this.setState({
+            selectedPostId: id
+        })
     }
 
     render () {
 
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title}></Post>
+            return <Post 
+            key={post.id} 
+            title={post.title} 
+            author={post.author}
+            clicked={()=> this.postSelectedHandler(post.id)}
+            ></Post>
         })
+        
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
