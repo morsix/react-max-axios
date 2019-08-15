3a1a511b71ee63265178de214186abf55d20006c
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-15 14:16:34 +0100

    added new post route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route }from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './../Posts/Posts';
+import NewPost from '../NewPost/NewPost';
 
 class Blog extends Component {
     render() {
@@ -15,7 +17,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts></Posts>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>}></Route> */}
+                <Route path="/" exact component={Posts} ></Route>
+                <Route path="/new-post" component={NewPost} ></Route>
             </div>
         );
     }
