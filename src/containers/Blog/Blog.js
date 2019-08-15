6442090051c68c37db5e52676433f889c178f9ef
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-15 13:47:06 +0100

    restructered the project for navigation purposes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import axiosInstance from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -65,15 +62,6 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
             </div>
         );
     }
---
+++ src/containers/Posts/Posts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const pors = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Posts
